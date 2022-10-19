e40cab32d5d246053131d6df193ae4ba848a0da5
DeanPeter Claassen <deanpeter8162@gmail.com>
2022-10-19 16:40:13 -0400

    cool stuff

---
+++ public/main.js
@@ -0,0 +1,6 @@
+let invite = document.querySelector('.buy')
+
+const message= evt =>{
+    alert("We know where you are. We already charged you card. There is no stopping us")
+}
+invite.addEventListener('click',message)
