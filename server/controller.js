a2215d9752708bde6723e69009a22e25d12763d6
DeanPeter Claassen <deanpeter8162@gmail.com>
2022-10-20 15:33:56 -0400

    add rollbar to server

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,11 @@
 
 
 const path = require('path')
+
+
 module.exports={
     getHTML: (req,res)=>{
+        rollbar.info("poop")
         res.sendFile(path.join(__dirname, '../public/index.html'))
 
     }
